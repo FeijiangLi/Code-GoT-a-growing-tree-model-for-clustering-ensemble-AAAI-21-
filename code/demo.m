d28055135a383c8b2255aa5ce1ccd697889479a4
FeijiangLi <166470886+FeijiangLi@users.noreply.github.com>
2024-04-09 12:20:25 +0800

    Delete code directory

--- code/demo.m
+++
@@ -1,9 +0,0 @@
-load Iris.mat
-gt=data(:,end);
-H=51;
-k=length(unique(gt));
-data_feature=data(:,1:end-1);
-% data_feature=predata(data_feature);
-[clusterings] =creat_clusters_fixk_kmeans(data_feature,H);
-result=GoT(clusterings,k );
-[ac,ARI,NMI]=evaluate2(result,gt,k)
